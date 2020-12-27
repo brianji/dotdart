962848c980af2b908f3dc00107e2bcefb0fa1b91
Brian Isganitis <brian@bmac.localdomain>
2020-12-26 19:10:47 -0500

    Adjust used dot files and update files.

--- bin/dotdart.dart
+++ bin/dotdart.dart
@@ -2,20 +2,20 @@ import 'package:dotdart/dotdart.dart';
 import 'package:dotdart/file.dart';
 
 const files = [
-  DotFile.config(directory: 'bspwm', name: 'bspwmrc'),
-  DotFile.config(directory: 'dunst', name: 'dunstrc'),
-  DotFile.config(directory: 'kitty', name: 'kitty.conf'),
-  DotFile.config(directory: 'sxhkd', name: 'sxhkdrc'),
-  DotFile.config(directory: 'rofi', name: 'config.rasi'),
+  //DotFile.config(directory: 'bspwm', name: 'bspwmrc'),
+  //DotFile.config(directory: 'dunst', name: 'dunstrc'),
+  //DotFile.config(directory: 'kitty', name: 'kitty.conf'),
+  //DotFile.config(directory: 'picom', name: 'picom.conf'),
+  //DotFile.config(directory: 'rofi', name: 'config.rasi'),
+  //DotFile.config(directory: 'sxhkd', name: 'sxhkdrc'),
+  //DotFile.home(name: 'Xmodmap'),
+  //DotFile.home(name: 'asoundrc'),
+  //DotFile.home(name: 'xinitrc'),
   DotFile.config(directory: 'pacman', name: 'makepkg.conf'),
-  DotFile.config(directory: 'picom', name: 'picom.conf'),
-  DotFile.home(name: 'asoundrc'),
   DotFile.home(name: 'bash_profile'),
   DotFile.home(name: 'bashrc'),
   DotFile.home(name: 'inputrc'),
   DotFile.home(name: 'vimrc'),
-  DotFile.home(name: 'xinitrc'),
-  DotFile.home(name: 'Xmodmap'),
 ];
 
 void main() => install(files);
